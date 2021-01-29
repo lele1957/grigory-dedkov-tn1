f77eef0f356398416a76fa6fe96a9c6e252c60ac
Grigory <grishkaded1@gmail.com>
2021-01-30 01:48:01 +0300

    first commit

---
+++ tn1.rb
@@ -0,0 +1,12 @@
+puts "Введите ваше имя"
+name = gets.chomp
+puts "Введите ваш рост"
+height = gets.chomp
+if ("#{height}".to_i - 110) * 1.15 < 0 
+	puts "Ваш вес уже оптимальный"
+else
+	puts "#{name}, ваш вес #{("#{height}".to_i - 110) * 1.15} " 
+=begin
+.capitalize перед именем почему не работает 	
+=end
+end 	
