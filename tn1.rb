e31ad1edb04ae65567ee600afc672bf681f12d6f
lele1957 <75094755+lele1957@users.noreply.github.com>
2021-02-02 00:34:52 +0300

    Add files via upload

--- tn1.rb
+++ tn1.rb
@@ -3,10 +3,10 @@ name = gets.chomp
 puts "Введите ваш рост"
 height = gets.chomp.to_i
 if ("#{height}" - 110) * 1.15 < 0 
-	puts "Ваш вес уже оптимальный"
+  puts "Ваш вес уже оптимальный"
 else
-	puts "#{name}, ваш вес #{("#{height}".to_i - 110) * 1.15} " 
+  puts "#{name}, ваш вес #{("#{height}".to_i - 110) * 1.15} " 
 =begin
-.capitalize перед именем почему не работает 	
+.capitalize перед именем почему не работает   
 =end
-end 	
+end   
