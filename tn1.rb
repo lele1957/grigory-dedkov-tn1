f42ab0ad3280f985e253f1d8320330c1c01a06de
Grigory <grishkaded1@gmail.com>
2021-02-01 20:05:26 +0300

    first commit

--- tn1.rb
+++ tn1.rb
@@ -1,8 +1,8 @@
 puts "Введите ваше имя"
 name = gets.chomp
 puts "Введите ваш рост"
-height = gets.chomp
-if ("#{height}".to_i - 110) * 1.15 < 0 
+height = gets.chomp.to_i
+if ("#{height}" - 110) * 1.15 < 0 
 	puts "Ваш вес уже оптимальный"
 else
 	puts "#{name}, ваш вес #{("#{height}".to_i - 110) * 1.15} " 
